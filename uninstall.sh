b07146e3cc2a4ba7f97c46120e963627c182a66a
Angelo Santangelo <asantangelo11@gmail.com>
2024-02-01 00:24:05 +0100

    Fix bash files

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,7 @@
 RESOURCE_GROUP="piscoo"
+STREAM_JOB="pisco_job"
+
+az stream-analytics job stop --job-name $STREAM_JOB --resource-group $RESOURCE_GROUP
 
 echo "Uninstalling resource group $RESOURCE_GROUP ..."
 az group delete --name $RESOURCE_GROUP --yes
