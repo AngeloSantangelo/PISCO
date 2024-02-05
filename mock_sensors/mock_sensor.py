5769bd5fbf076faa1979d5e1f753684cb2247501
Angelo Santangelo <asantangelo11@gmail.com>
2024-02-05 19:21:09 +0100

    Fix mock sensor

--- mock_sensors/mock_sensor.py
+++ mock_sensors/mock_sensor.py
@@ -4,44 +4,32 @@ import time
 from azure.iot.device import IoTHubDeviceClient, Message
 
 idSensor = 3
-
 MSG_TXT = '{{"peopleNumber": {number}, "idSensor": {idSensor}}}'
 
-
-def run_telemetry_sample(client):
+def simulate_sensor(client):
     
     print("IoT Hub device sending periodic messages")
 
     client.connect()
 
-    
     number = random.randint(1, 100)
     msg_txt_formatted = MSG_TXT.format(number=number, idSensor=idSensor)
     message = Message(msg_txt_formatted)
-
-    # if number > 30:
-    #     message.custom_properties["temperatureAlert"] = "true"
-    # else:
-    #     message.custom_properties["temperatureAlert"] = "false"
 
     # Send the message.
     print("Sending message: {}".format(message))
     client.send_message(message)
     print("Message successfully sent")
-    
-    #time.sleep(10)
-
 
 def main():
-    print("IoT Hub Quickstart #1 - Simulated device")
     print("Press Ctrl-C to exit")
 
     client = IoTHubDeviceClient.create_from_connection_string("HostName=piscooiothub.azure-devices.net;DeviceId=3;SharedAccessKey=VzbXTMS2fHYhr2tI3HaJe4YmwatbbzWl7AIoTMG2ojw=")
 
     try:
-        run_telemetry_sample(client)
+        simulate_sensor(client)
     except KeyboardInterrupt:
-        print("IoTHubClient sample stopped by user")
+        print("IoTHubClient stopped by user")
     finally:
         print("Shutting down IoTHubClient")
         client.shutdown()
