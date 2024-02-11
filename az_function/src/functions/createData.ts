02365b3cd36904640f0808e32b81d217de603491
Angelo Santangelo <asantangelo11@gmail.com>
2024-02-11 17:51:29 +0100

    Final fix

--- az_function/src/functions/createData.ts
+++ az_function/src/functions/createData.ts
@@ -11,35 +11,21 @@ export async function createData(request: HttpRequest, context: InvocationContex
         const peopleNumber = data.peopleNumber;
         const idSensor = data.idSensor;
 
-        // for(let month = 1; month <= 12; month++)
-        // {
-        //     for(let j=0; j<10; j++)
-        //     {
-        //         const date = new Date(`2024-${month.toString().padStart(2, '0')}-${(j+1).toString().padStart(2, '0')}T18:25:26.216Z`);
-        //         const collectedData = await prisma.collectedData.create({
-        //             data: {
-        //                 peopleNumber: peopleNumber,
-        //                 idSensor: idSensor,
-        //                 date: date
-        //             }
-        //         });
+        for(let month = 1; month <= 12; month++)
+        {
+            for(let j=0; j<10; j++)
+            {
+                const date = new Date(`2024-${month.toString().padStart(2, '0')}-${(j+1).toString().padStart(2, '0')}T18:25:26.216Z`);
+                const collectedData = await prisma.collectedData.create({
+                    data: {
+                        peopleNumber: peopleNumber,
+                        idSensor: idSensor,
+                        date: date
+                    }
+                });
                 
-        //     }
-        // }
-
-        const collectedData = await prisma.collectedData.create({
-            data: {
-                peopleNumber: peopleNumber,
-                idSensor: idSensor
             }
-        });
-
-        return {
-            jsonBody: {
-                collectedData
-            },
-        };
-        
+        }
     } catch (error) {
         console.error('Error processing request:', error);
         return { body: error };
